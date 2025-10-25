9080831013fe4c9def6ff89eb62d0a378b634796
Vijay Jaiswal <vijay.jaiswal@qio.io>
2025-10-25 11:22:23 +0530

    Initial commit: PII Scanning Example

--- Chapter_01/PII_Scanning_Names.py
+++ Chapter_01/PII_Scanning_Names.py
@@ -1,10 +1,12 @@
+# ------------------------------------------------------------
+# Basic PII Scanning in Fake People Dataset
+# ------------------------------------------------------------
 import pandas as pd
-import numpy as np
 
-# Load a sample dataset (e.g., an employee file)
 # Downloaded and renamed from https://www.kaggle.com/datasets/shuvokumarbasak4004/fake-dataset-for-practice?select=datainfo.csv
 
-df = pd.read_csv('D:\\Vijay_Personal\\workspace\\Projects\\Author_Books\\Code\\PPA-Techniques_for_Secure_Data_Analysis\\Chapter_01\data\\fake_people_datainfo.csv')
+# Load a sample dataset (e.g., fake_people_datainfo file)
+df = pd.read_csv('.\\data\\fake_people_datainfo.csv')
 
 # Simple check for potential PII columns based on column names
 pii_keywords = ['name', 'email', 'ssn', 'address', 'phone', 'dob']
@@ -12,8 +14,9 @@ potential_pii_columns = []
 
 for column in df.columns:
     for keyword in pii_keywords:
+        # Match for exact name
         if keyword in column.lower():
             potential_pii_columns.append(column)
             break
-
+# Print the result
 print("Potential PII columns based on names:", potential_pii_columns)
